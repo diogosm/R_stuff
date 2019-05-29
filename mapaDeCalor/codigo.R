1d98f9aa4de1960c72b67a39a90987f388107333
diogosm <dsmozh@gmail.com>
2019-05-29 13:39:54 -0400

    bug fixes, generalizacoes e upgrades no mapa de calor

--- mapaDeCalor/codigo.R
+++ mapaDeCalor/codigo.R
@@ -1,9 +1,13 @@
-if(!require("gplots")) install.packages("gplots")
-if(!require("lattice")) install.packages("lattice")
-if(!require("raster")) install.packages("raster")
+if(!require(gplots)) install.packages(gplots)
+if(!require(lattice)) install.packages(lattice)
+if(!require(raster)) install.packages("raster")
+if(!require(grid)) install.packages(grid)
+if(!require("RColorBrewer")) install.packages("RColorBrewer")
 
 library(gplots)
 library(lattice)
+library(grid)
+library(RColorBrewer)
 require(raster)
 
 DIM <- function( ... ){
@@ -13,15 +17,16 @@ DIM <- function( ... ){
                                  dim(x) } )
 }
 
-data=as.matrix(read.table("input.txt",header=T,sep="\t",row.names=1,as.is=T))
+tabela=read.table("input.txt",header=T,sep="\t",row.names=1,as.is=T)
+data=as.matrix(tabela)
 nomes=read.table("input.txt", header=T,sep="\t")
 #data=t(data)
 #nomes=t(nomes)
 
-rowColNames=list(nomes$Records,as.character(2010:2017))
+rowColNames=list(nomes$Records,as.character(names(tabela)))
 #rowColNames=list(as.character(2010:2017),nomes$Records)
 #matriz=matrix(data, ncol=10,byrow=T,dimnames=rowColNames)
-matriz=matrix(data, ncol=8,byrow=F,dimnames=rowColNames)
+matriz=matrix(data, ncol=length(names(tabela)),byrow=F,dimnames=rowColNames)
 
 cols <- rev(colorRampPalette(brewer.pal(6, "RdYlGn"))(20))
 
